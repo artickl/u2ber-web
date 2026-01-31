bbd46740b003c7ebeeb3916e802dbdd8ed2a03be
artickl <artem.zavyalov@gmail.com>
2026-01-31 00:22:16 +0000

    clean up output from cronjob restart

--- run.sh
+++ run.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-docker compose up --build -d
+
+#if from cronjob, remove output redirection
+if [[ ! -t 0 ]]; then
+    docker compose up --build -d >/dev/null 2>&1
+else
+    docker compose up --build -d
+fi
+
+#check status
+docker compose ps
